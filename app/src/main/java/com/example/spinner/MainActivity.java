bd5fbe3632705ec44fe7a475dc1f42983ec3bc7d
Nour-elgouhary <nnoouurr60@gmail.com>
2020-06-16 23:24:11 +0200

    change

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.spinner;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +20,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+        TextView textView = findViewById(R.id.nooid);
 
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+    }
+
+    //make alert dialog
+    public void Click(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("ALERT");
+        builder.setMessage("Confirm The Order Or Cancel It If You Want .... You Can Open Chat With Us In WhatsApp  ");
+        //confirm button
+        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "yes Order", Toast.LENGTH_LONG).show();
+            }
+        });
+        // cancel button
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Cancel Order", Toast.LENGTH_LONG).show();
+                dialog.cancel();
+
+            }
+        });
+
+        //chat button
+        builder.setNeutralButton("Contact Us ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String num = "01222262117";
+                Uri uri = Uri.parse("smsto:" + num);
+                Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+                i.setPackage("com.whatsapp");
+                startActivity(i);
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
